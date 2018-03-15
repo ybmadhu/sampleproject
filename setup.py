02e23ad9d815f5a2931685850f41ce9335455a61
ybmadhu <ybmadhu707@gmail.com>
2018-03-15 17:09:44 +0530

    edit setup.py

--- setup.py
+++ setup.py
@@ -176,9 +176,7 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
-        'Funding': 'https://donate.pypi.org',
-        'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/pypa/sampleproject/',
+        
+        'Source': 'https://github.com/ybmadhu/sampleproject/',
     },
 )
